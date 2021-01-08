54fbc8c45b0b8c9a4a1459de5e384054031ff15b
Kashish Khullar <Kashish Khullar@LAPTOP-PD58OTN0.(none)>
2021-01-09 01:21:35 +0530

    updated clipper dimensions

--- lib/helpers/home.clipper.dart
+++ lib/helpers/home.clipper.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HomeClipper extends CustomClipper<Path> {
+class TopClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var controlPoint = Offset(size.width / 2, size.height);
 
     Path path = Path();
-    path.lineTo(0, size.height / 2);
+    path.lineTo(0, size.height * 0.55);
     path.quadraticBezierTo(
-        controlPoint.dx, controlPoint.dy, size.width, size.height / 2);
+        controlPoint.dx, controlPoint.dy, size.width, size.height * 0.55);
     path.lineTo(size.width, 0);
 
     return path;
