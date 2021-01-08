3826758c83ba6888c1b36bf655b41c95ab961e4d
Kashish Khullar <Kashish Khullar@LAPTOP-PD58OTN0.(none)>
2021-01-09 01:21:11 +0530

    added class for global constants

---
+++ lib/config/global_strings.config.dart
@@ -0,0 +1,22 @@
+class GlobalStrings {
+  GlobalStrings._();
+
+  static const String aboutGita =
+      "महाभारत के अनुसार कुरुक्षेत्र युद्ध में भगवान श्री कृष्ण ने गीता का सन्देश अर्जुन को सुनाया था।\nयह महाभारत के भीष्मपर्व के अन्तर्गत दिया गया एक उपनिषद् है।\nभगवत गीता में एकेश्वरवाद, कर्म योग, ज्ञानयोग, भक्ति योग की बहुत सुन्दर ढंग से चर्चा हुई है।";
+
+  static const String srimadBhagwatGita = "श्रीमद्भगवद्गीता";
+
+  static const String chapterList = "अध्यायों की सूची";
+
+  static const String chapter = "अध्याय";
+
+  static const String verseList = "श्लोकों की सूची";
+
+  static const String verse = "श्लोक";
+
+  static const String panelText = "अनुवाद और अर्थ पढ़ने के लिए ऊपर खिचिये";
+
+  static const String meaning = "शब्दार्थ";
+
+  static const String translation = "अनुवाद";
+}
